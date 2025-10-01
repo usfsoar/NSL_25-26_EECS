688c95914a62c9d465997cd9fec4ea0c37abd740
Khanh Mai <khanhmai@usf.edu>
2025-09-30 22:35:52 -0400

    Working GPS code from last year. Tested with recent modules successfully.

---
+++ Telemetry/Telemetry_Testing_Dev/GPS_I2C/V1_SOAR_RTOS_GPS.h
@@ -0,0 +1,20 @@
+#ifndef SOAR_RTOS_GPS_h
+#define SOAR_RTOS_GPS_h
+#include <Wire.h>
+#include <Adafruit_GPS.h>
+
+class SOAR_RTOS_GPS {
+  public:
+    SOAR_RTOS_GPS(); // Constructor
+    void setup();
+    char* GET_NMEA(char* nmea);
+
+    private:
+      Adafruit_GPS GPS;  // GPS
+      uint32_t GPS_FOCUS_MAX = 5000;
+    // Declare private variables and methods here
+};
+  
+
+
+#endif
