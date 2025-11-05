3b0449fc76cb8fc54f1832ed5737e50cfc3f8241
Khanh Mai <khanhmai@usf.edu>
2025-11-04 21:46:55 -0500

    Working SD card code. Write to 2 SD cards at the same time.

---
+++ Telemetry/Telemetry_Testing_Dev/SD_Card_Store/Updated_SD_Card_Store/_config.h
@@ -0,0 +1,7 @@
+#define QUEUE_LEN 10
+#define QUEUE_SEND_DELAY 1000 // Delays in ms
+#define QUEUE_RECEIVE_DELAY 1000
+
+const char* IMU_FILEPATH = "/imu.csv";
+const char* ALTIMETER_FILEPATH = "/altimeter.csv";
+const char* LORA_FILEPATH = "/lora.csv";
