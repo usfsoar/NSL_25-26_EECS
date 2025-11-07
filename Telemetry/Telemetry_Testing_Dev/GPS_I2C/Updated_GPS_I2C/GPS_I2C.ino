f69e10207a2cd2ded1ed2ee9ac187bfcc8fe9118
Khanh Mai <khanhmai@usf.edu>
2025-11-06 21:00:57 -0500

    Woking GPS code

---
+++ Telemetry/Telemetry_Testing_Dev/GPS_I2C/Updated_GPS_I2C/GPS_I2C.ino
@@ -0,0 +1,42 @@
+#include "V1_SOAR_RTOS_GPS.h"
+// #define GPSECHO false
+
+SOAR_RTOS_GPS gps2;
+Adafruit_GPS GPS(&Wire);
+
+void GPSTask (void *Parameters) {
+  while(1){
+
+    //  read data from the GPS in the 'main loop'
+  // char c = GPS.read();
+  // // if you want to debug, this is a good time to do it!
+  // if (GPSECHO)
+  //   if (c) Serial.print(c);
+  // // if a sentence is received, we can check the checksum, parse it...
+  // if (GPS.newNMEAreceived()) {
+  //   // a tricky thing here is if we print the NMEA sentence, or data
+  //   // we end up not listening and catching other sentences!
+  //   // so be very wary if using OUTPUT_ALLDATA and trying to print out data
+  //   Serial.println(GPS.lastNMEA()); // this also sets the newNMEAreceived() flag to false
+  // }
+      char nmea[100];
+      gps2.GET_NMEA(nmea);
+      Serial.print("NMEA data: ");
+      Serial.println(nmea);
+    }
+  }
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  Wire.setClock(100000);
+  gps2.setup();
+}
+
+void loop() {
+  char nmea[100];
+  gps2.GET_NMEA(nmea);
+  Serial.print("NMEA data: ");
+  Serial.println(nmea);
+  delay(1000);
+}
