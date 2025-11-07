23c9469055b5efc17a8b7e99dd62c9fc296bc366
Somewhat-average <100049140+Somewhat-average@users.noreply.github.com>
2025-11-06 20:56:52 -0500

    removed rtos code

--- Telemetry/Telemetry_Testing_Dev/SOAR_BMP581/SOAR_BMP581.ino
+++ Telemetry/Telemetry_Testing_Dev/SOAR_BMP581/SOAR_BMP581.ino
@@ -23,38 +23,8 @@ void setup() {
 
     barometer.begin();
     delay(1000);
-    // vTaskDelay(1000 - portTICK_PERIOD_MS);
     i_altitude = barometer.get_altitude();
-    // xTaskCreate(
-    //     BMP581Task,
-    //     "BMP581Task",
-    //     10000,
-    //     NULL,
-    //     1,
-    //     NULL
-    // );
 }
-
-// void BMP581Task(void *parameters) {
-//     while (1) {
-//         altitude = barometer.get_altitude() - i_altitude;
-//         pressure = barometer.get_pressure();
-//         temperature = barometer.get_temperature();
-        
-//         Serial.println("Altitude: " + String(altitude));
-        
-//         if (altitude == 0) {
-//             Serial.println("Altitude is 0, retrying...");
-//             barometer.begin();
-//         } 
-        
-//         Serial.println("Pressure: " + String(pressure));
-//         Serial.println("Temperature: " + String(temperature));
-//         Serial.println("--------------------------------");
-//         delay(1000);
-//     }
-// }
-
 
 void loop() {
     altitude = barometer.get_altitude() - i_altitude;
