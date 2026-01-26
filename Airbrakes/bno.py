238b31f1c7cb14252f8c372d2971cf41dbfec9e3
chfernandez05 <christopherf20@usf.edu>
2026-01-26 12:15:28 -0500

    Updated sensor classes with test code

--- Airbrakes/bno.py
+++ Airbrakes/bno.py
@@ -4,24 +4,33 @@ import board
 import busio
 import adafruit_bno08x
 from adafruit_bno08x.i2c import BNO08X_I2C
+from adafruit_bno08x import (
+    BNO_REPORT_ACCELEROMETER,
+    BNO_REPORT_GYROSCOPE,
+    BNO_REPORT_MAGNETOMETER,
+    BNO_REPORT_ROTATION_VECTOR,
+    BNO_REPORT_LINEAR_ACCELERATION,
+    BNO_REPORT_GRAVITY
+)
 
 
-class BNO():
+class BNO:
     def __init__(self):
         self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.sensor = BNO08X_I2C(self)
+        self.sensor = BNO08X_I2C(self.i2c)
 
-    def temperature(self):
-        return self.sensor.temperature
-    
+        self.sensor.enable_feature(BNO_REPORT_ACCELEROMETER)
+        self.sensor.enable_feature(BNO_REPORT_MAGNETOMETER)
+        self.sensor.enable_feature(BNO_REPORT_ROTATION_VECTOR)
+        self.sensor.enable_feature(BNO_REPORT_LINEAR_ACCELERATION)
+        self.sensor.enable_feature(BNO_REPORT_GRAVITY)
+
+
     def magnetic(self):
         return self.sensor.magnetic
     
-    def gyro(self):
-        return self.sensor.gyro
-    
-    def euler(self):
-        return self.sensor.euler
+    # def gyro(self):
+    #     return self.sensor.gyro
     
     def quaternion(self):
         return self.sensor.quaternion
@@ -36,4 +45,14 @@ class BNO():
         return self.sensor.gravity
 
 if __name__ == '__main__':
-    pass
+    import time
+    bno = BNO()
+    print('Created bno object')
+    while(1):
+        print(f'{bno.magnetic()=}')
+        # print(f'{bno.gyro()=}')
+        print(f'{bno.quaternion()=}')
+        print(f'{bno.linear_acceleration()=}')
+        print(f'{bno.acceleration()=}')
+        print(f'{bno.gravity()=}')
+        time.sleep(2)
