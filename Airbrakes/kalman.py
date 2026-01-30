9c2dd8026c9810680af000ebbec4711427652362
chfernandez05 <christopherf20@usf.edu>
2026-01-30 17:35:36 -0500

    started kalman filter but like not really

--- Airbrakes/kalman.py
+++ Airbrakes/kalman.py
@@ -1,6 +1,10 @@
+import numpy
 # Class to filter out noisy data
-class KalmanFilter():
-    pass
-
-if __name__ == '__main__':
-    pass
+class KalmanFilter:
+    def __init__(self):
+        # self.state_transition = None
+        # self.observation = None
+        # self.process_noise = None
+        # self.observation_noise = None
+        # self.
+        pass
