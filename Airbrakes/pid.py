3d060079d689d06161bb47d8c8dd7e61bc593601
Dereck Santiago <dereckprstgo@yahoo.com>
2026-02-18 20:11:29 -0500

    fixed clamp and added few fixes to pidSum and initial dt

--- Airbrakes/pid.py
+++ Airbrakes/pid.py
@@ -18,7 +18,7 @@ class PID:
         self.minSteps = 0
         self.maxSteps = 60 #6000/100 NEEDS TO BE REVISED
 
-        self.dt = 0
+        self.dt = 1
         self.derivativeValue = 0
         self.integralValue = 0
         self.errorValue = 0
@@ -52,7 +52,7 @@ class PID:
         return self.derivativeValue
 
     def pidSum(self):
-        self.update(self, self.time, self.acceleration, self.altitude, self.velocity)
+        #self.update(self, self.time, self.acceleration, self.altitude, self.velocity)
         self.pidOutput = self.proportionalValue + self.integralValue + self.derivativeValue
         return self.pidOutput
             
@@ -60,13 +60,13 @@ class PID:
         pass # we need the conversion rate from pid output to formula
 
     def clamp(self, inputSteps): #inputSteps being the number of steps the code demands
-        resetIntegral = False
+        resetIntegral = True
 
-        if (inputSteps <= self.minSteps or inputSteps >= self.maxSteps):
-            resetIntegral = True
+        if (inputSteps >= self.minSteps or inputSteps <= self.maxSteps):
+            resetIntegral = False
 
-        if (self.errorValue > 0 and self.pidOutput < 0) or (self.errorValue < 0 and self.pidOutput > 0):
-            resetIntegral = True
+        if (self.errorValue > 0 and self.pidOutput > 0) or (self.errorValue < 0 and self.pidOutput < 0):
+            resetIntegral = False
 
         if resetIntegral == True:
             self.integralValue = 0
