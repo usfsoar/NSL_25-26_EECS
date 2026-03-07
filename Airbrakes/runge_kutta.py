70b45415b1bc82391a4de3900d1a51affe36e73f
1SamXD <johnson.samuel2005@gmail.com>
2026-03-06 20:24:13 -0500

    added rk4 class, updated pid class

--- Airbrakes/runge_kutta.py
+++ Airbrakes/runge_kutta.py
@@ -1,28 +1,21 @@
-import numpy as np
-import math
-# Function to predict the rocket's apogee
+class RungeKutta4:
+    def __init__(self):
+        self.dt = 0.1
 
-def f(t, a, v):
-            accel = -9.8
-            accel = d / 20 - 9.8 #20 is the assumed mass
-            return np.array([v, accel])
-def prediction(time, altitude, velocity, acceleration):
-    dt = 0.1
+    def step(self, time, altitude, velocity, acceleration):
+        vel = velocity
+        dt = self.dt
+        while velocity > 0:
+            v1, a1 = velocity, acceleration
+            v2, a2 = velocity + a1 * dt/2, acceleration
+            v3, a3 = velocity + a2 * dt/2, acceleration
+            v4, a4 = velocity + a3 * dt, acceleration
+
+            altitude += dt/6 * (v1 + 2*v2 + 2*v3 + v4)
+            velocity += dt/6 * (a1 + 2*a2 + 2*a3 + a4)
+            time += dt
+
+        return altitude
     
-    while velocity > 0:
-        
-        
-        k1 = f(time, altitude, velocity)
-        k2 = f(time + dt/2, altitude + k1[0] * dt/2, velocity + k1[1] * dt/2)
-        k3 = f(time + dt/2, altitude + k2[0] * dt/2, velocity + k2[1] * dt/2)
-        k4 = f(time + dt, altitude + k3[0] * dt, velocity + k3[1] * dt)
-
-        altitude += (dt/6) * (k1[0] + 2*k2[0] + 2*k3[0] + k4[0])
-        velocity += (dt/6) * (k1[1] + 2*k2[1] + 2*k3[1] + k4[1])
-        time += dt
-    return altitude
-
-
-
 if __name__ == '__main__':
     pass
