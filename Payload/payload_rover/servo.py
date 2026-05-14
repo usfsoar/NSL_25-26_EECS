631c61dbf3d17cf5df816b511a92f63e38a4a6cf
1SamXD <johnson.samuel2005@gmail.com>
2026-05-14 17:56:49 -0400

    servo working code

--- Payload/payload_rover/servo.py
+++ Payload/payload_rover/servo.py
@@ -1,30 +1,34 @@
-from gpiozero import AngularServo
-from time import sleep
-
-servo = AngularServo(14, min_angle=-180, max_angle=180)
-
-while True:
-    servo.angle = -180
-    sleep(2)
-    servo.angle = -90
-    sleep(2)
-    servo.angle = 0
-    sleep(2)
-    servo.angle = 90
-    sleep(2)
-    servo.angle = 180
-    sleep(2)
-
-
-# from gpiozero import Servo
+# from gpiozero import AngularServo
 # from time import sleep
 
-# servo = Servo(14)
+# servo = AngularServo(12, min_angle=-180, max_angle=180)
 
 # while True:
-#     servo.min()
+#     servo.angle = -180
 #     sleep(2)
-#     servo.mid()
+#     servo.angle = -90
 #     sleep(2)
-#     servo.max()
+#     servo.angle = 0
 #     sleep(2)
+#     servo.angle = 90
+#     sleep(2)
+#     servo.angle = 180
+#     sleep(2)
+
+
+from gpiozero import Servo
+from time import sleep
+
+servo = Servo(18, min_pulse_width=0.0005, max_pulse_width=0.0025)
+
+while True:
+    print("min")
+    servo.value = -1
+    sleep(2)
+    print("min")
+    servo.value = 0
+    sleep(2)
+    print("min")
+    servo.value = 1
+    sleep(2)
+
