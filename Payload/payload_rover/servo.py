fc43b09688ecefcc8ade397cca3f41b789a4b0e6
1SamXD <johnson.samuel2005@gmail.com>
2026-05-15 19:43:00 -0400

    servo class

--- Payload/payload_rover/servo.py
+++ Payload/payload_rover/servo.py
@@ -1,34 +1,33 @@
-# from gpiozero import AngularServo
-# from time import sleep
-
-# servo = AngularServo(12, min_angle=-180, max_angle=180)
-
-# while True:
-#     servo.angle = -180
-#     sleep(2)
-#     servo.angle = -90
-#     sleep(2)
-#     servo.angle = 0
-#     sleep(2)
-#     servo.angle = 90
-#     sleep(2)
-#     servo.angle = 180
-#     sleep(2)
-
-
 from gpiozero import Servo
-from time import sleep
+import time
 
-servo = Servo(18, min_pulse_width=0.0005, max_pulse_width=0.0025)
+class ServoControl:
+    def __init__(self):
+        pass
 
-while True:
-    print("min")
-    servo.value = -1
-    sleep(2)
-    print("min")
-    servo.value = 0
-    sleep(2)
-    print("min")
-    servo.value = 1
-    sleep(2)
+    def initialize(self, pin: int = 18, min_angle: int = -130, max_angle: int = 130):
+        self.min = min_angle/135
+        self.max = max_angle/135
+        self.servo = Servo(pin, min_pulse_width=0.0005, max_pulse_width=0.0025)
 
+    def retract(self):
+        self.servo.value = self.max
+    
+    def lock(self):
+        self.servo.value = self.min
+
+if __name__ == '__main__':
+    servo = ServoControl()
+
+    servo.initialize(18, 0, 100)
+    time.sleep(2)
+    print("lock")
+    servo.lock()
+    time.sleep(5)
+    print("retract")
+    servo.retract()
+    time.sleep(5)
+
+    print("lock loop")
+    while True:
+        servo.lock()
