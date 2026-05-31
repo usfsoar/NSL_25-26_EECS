de7a3491d742bb79a9b2b31c043be91f663d2ba5
1SamXD <johnson.samuel2005@gmail.com>
2026-05-31 18:31:44 -0400

    created tof sensor

--- Payload/payload_sensor/tofvl53.py
+++ Payload/payload_sensor/tofvl53.py
@@ -13,34 +13,70 @@ import board
 
 import vl53l4cx
 
-i2c = board.I2C()  # uses board.SCL and board.SDA
-#i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+# i2c = board.I2C()  # uses board.SCL and board.SDA
+# #i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 
-vl53 = vl53l4cx.VL53L4CX(i2c)
+# vl53 = vl53l4cx.VL53L4CX(i2c)
 
-# OPTIONAL: can set non-default values
-vl53.distance_mode = 2
-vl53.timing_budget = 50
+# # OPTIONAL: can set non-default values
+# vl53.distance_mode = 2
+# vl53.timing_budget = 50
 
-print("VL53L4CX Simple Test.")
-print("--------------------")
-model_id, module_type, mask_rev = vl53.model_info
-print(f"Model ID: 0x{model_id:0X}")
-print(f"Module Type: 0x{module_type:0X}")
-print(f"Mask Revision: 0x{mask_rev:0X}")
-print("Distance Mode: ", end="")
-if vl53.distance_mode == 1:
-    print("SHORT")
-elif vl53.distance_mode == 2:
-    print("LONG")
-else:
-    print("UNKNOWN")
-print(f"Timing Budget: {vl53.timing_budget}")
-print("--------------------")
+# print("VL53L4CX Simple Test.")
+# print("--------------------")
+# model_id, module_type, mask_rev = vl53.model_info
+# print(f"Model ID: 0x{model_id:0X}")
+# print(f"Module Type: 0x{module_type:0X}")
+# print(f"Mask Revision: 0x{mask_rev:0X}")
 
-vl53.start_ranging()
+# print("Distance Mode: ", end="")
+# if vl53.distance_mode == 1:
+#     print("SHORT")
+# elif vl53.distance_mode == 2:
+#     print("LONG")
+# else:
+#     print("UNKNOWN")
+# print(f"Timing Budget: {vl53.timing_budget}")
+# print("--------------------")
 
-while True:
-    if vl53.data_ready:
-        print(f"Dist: {vl53.distance} cm, Sig: {vl53.sigma} cm, Stat: {vl53.range_status}")
-        vl53.clear_interrupt()
+# vl53.start_ranging()
+
+# while True:
+#     if vl53.data_ready:
+#         print(f"Dist: {vl53.distance} cm, Sig: {vl53.sigma} cm, Stat: {vl53.range_status}")
+#         vl53.clear_interrupt()
+
+class TOF():
+    def __init__(self):
+        pass
+
+    def initialize(self, address):
+        for i in range(10):
+            try:
+                self.i2c = board.I2C()
+                break
+            except Exception as e:
+                print(f"Error initializing I2C for TOF: {e}")
+        for i in range(10):
+            try:
+                self.sensor = vl53l4cx.VL53L4CX(self.i2c)
+                break
+            except Exception as e:
+                print(f"Error initializing TOF sensor: {e}")
+        
+        self.sensor.distance_mode = 2
+        self.sensor.timing_budget = 50
+
+        self.sensor.start_ranging()
+    
+    def get_distance(self):
+        distance = self.sensor.distance
+        self.sensor.clear_interrupt()
+        return distance
+
+if __name__ == "__main__":
+    tof = TOF()
+    tof.initialize()
+    while True:
+        print(tof.get_distance())
+        time.sleep(0.1)
